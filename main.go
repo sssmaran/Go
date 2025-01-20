4147c6c2f07aa5fb03c5b6358f1c5e13fdd9eeb8
skota-hash <skota3@buffalo.edu>
2025-01-19 22:11:20 -0500

    local var

--- main.go
+++ main.go
@@ -4,18 +4,18 @@ import (
 	"fmt"
 )
 
-//Global variables
-var task1 = "Learn Go programming"
+func main() {
+    fmt.Println("Hello, World!")
+	//local variables
+	var task1 = "Learn Go programming"
 	var task2 = "Create a simple web application"
 	var task3 = "Create a backend server"
 	var tasks = []string{task1, task2, task3}
-func main() {
-    fmt.Println("Hello, World!")
-	taskslist()
+	taskslist(tasks)
 	}
 
-func taskslist(){
-	fmt.Println("List of Tasks:\n")
+func taskslist(tasks []string) {       //parameter passinng tasks list
+	fmt.Println("List of Tasks:")
 	for index, task := range tasks {
 		fmt.Printf("Task %d: %s\n", index+1, task)
     
